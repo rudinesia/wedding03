b2923ca2df60080fff627df72f8650cda7662341
Rudi Kurniawan <rudikurniawan@dinustek.com>
2019-12-02 21:17:23 +0700

    commit pertama

---
+++ README.md
@@ -0,0 +1,7 @@
+# Starter Template
+
+This template contain:
+- jquery
+- jquery.easing
+- popper.js
+- bootstrap 4.3.1
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+
+// Require jquery, fixing error jquery/$ not defined
+window.$ = window.jQuery = require('jquery');
+require('jquery.easing');
+
